8d2377546a597009558fe26f689354322e01d7f7
pyons04 <broad.basic15gmail.com>
2022-09-04 18:10:20 +0900

    add unitTest for detail.spec.js

---
+++ tests/unit/detail.spec.js
@@ -0,0 +1,41 @@
+import { mount } from '@vue/test-utils'
+import DetailView from '../../src/main/pages/DetailView/DetailView.vue'
+
+import axios from 'axios'
+import {nextTick} from 'vue'
+
+describe('Unit Test for DetailView.vue.', () => {
+
+  const mockRoute = {
+    params: {
+      id: 1
+    }
+  }
+
+  const mockDetailApi = [
+    { 
+      title:'AAA',
+      created_date:'2000-00-00',
+      category:'3',
+      description:'これは説明です',
+      status:'4',
+      deadlinedate:'2000-00-00',
+      lastupdatedate:'2000-00-00'
+    },
+  ]
+
+  jest.spyOn(axios, 'get').mockResolvedValue(mockDetailApi)
+
+  it('renders props.msg when passed', async() => {
+    const wrapper = mount(DetailView,{
+      global: {
+        mocks: {
+          $route: mockRoute,
+        }
+      }
+    })
+
+    expect(axios.get).toHaveBeenCalledTimes(1)
+    expect(axios.get).toHaveBeenCalledWith('/ticket/api/ticket/')
+  })
+})
