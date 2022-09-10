c8669595e4051da463c3a6486a018399f3af0a2b
pyons04 <broad.basic15gmail.com>
2022-09-10 14:00:13 +0900

    add new tests for DetailView

--- tests/unit/detail.spec.js
+++ tests/unit/detail.spec.js
@@ -1,4 +1,4 @@
-import { mount } from '@vue/test-utils'
+import { mount, flushPromises } from '@vue/test-utils'
 import DetailView from '../../src/main/pages/DetailView/DetailView.vue'
 
 import axios from 'axios'
@@ -14,10 +14,10 @@ describe('Unit Test for DetailView.vue.', () => {
 
   const mockDetailApi = {
     data: { 
-      title:'AAA',
+      title:'チケットの新規投稿に失敗する。',
       created_date:'2000-00-00',
       category:'3',
-      description:'これは説明です',
+      description:'チケットの新規投稿に失敗します。',
       status:'4',
       deadlinedate:'2000-00-00',
       lastupdatedate:'2000-00-00'
@@ -37,5 +37,12 @@ describe('Unit Test for DetailView.vue.', () => {
 
     expect(axios.get).toHaveBeenCalledTimes(1)
     expect(axios.get).toHaveBeenCalledWith('/ticket/api/ticket/1')
+
+    // Wait until the DOM updates.
+    await flushPromises()
+    //await nextTick
+    expect(wrapper.find('[data-testid="ticket-title"]').text()).toBe('チケットの新規投稿に失敗する。')
+    expect(wrapper.find('[data-testid="ticket-created_date"]').text()).toBe('2000-00-00')
+    expect(wrapper.find('[data-testid="ticket-category"]').text()).toBe('3')
   })
 })
