aaf8bc6396063566602a71610ad5034f6a36f79c
tinytony27 <tinytony.creator@gmail.com>
2022-11-06 14:14:19 +0900

    一覧のレイアウト調整

--- src/main/pages/ListView/ListView.vue
+++ src/main/pages/ListView/ListView.vue
@@ -15,24 +15,39 @@ export default {
       msg: 'API sample',
     };
   },
-  created () {
-    axios.get('/ticket/api/ticket')
-    .then( response => {
-      return response.data
-    })
-    .then( json => {
-      this.tickets = json
-    })
-    .catch((err) => {
-      this.msg = err // エラー処理
-    });
-  },
   mounted: function(){
     
     axios.defaults.xsrfCookieName = 'csrftoken'
     axios.defaults.xsrfHeaderName = "X-CSRFTOKEN"
     this.$store.dispatch('fetchStatusMaster');
     this.$store.dispatch('fetchCategoryMaster');
+
+    axios.get('/ticket/api/ticket')
+    .then( response => {
+      return response.data
+    })
+    .then( json => {
+      this.tickets = json
+      this.tickets.forEach(elem => {
+        for(let i = 0; i < this.$store.state.status.length; i++){
+          let status = this.$store.state.status[i];
+          if(status.id == elem.status){
+            elem.status = status.status;
+            break;
+          }
+        }
+        for(let i = 0; i < this.$store.state.categories.length; i++){
+          let category = this.$store.state.categories[i];
+          if(category.id == elem.category){
+            elem.category = category.category;
+            break;
+          }
+        }
+      });
+    })
+    .catch((err) => {
+      this.msg = err // エラー処理
+    });
   }
 }
 </script>
